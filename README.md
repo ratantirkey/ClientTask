7a0df444c5fbf9404cb544302d0c559f8b8870e4
ratan <ratan.tirkey@dreamorbit.com>
2023-09-16 17:22:40 +0530

    added search_json_value with specs and updated read me

--- README.md
+++ README.md
@@ -3,12 +3,14 @@
  1) run ruby search.rb
  2) run ruby search_api.rb
  3) run ruby search_json.rb
- 4) run rspec spec/* or run bundle exec rspec spec/*
+ 4) run ruby search_json_value.rb
+ 5) run rspec spec/* or run bundle exec rspec spec/*
 
 ## Table of contents
 * [search](#search-info)
 * [search_api](#search_api-info)
 * [search_json](#search_json-info)
+* [search_json_value](#search_json_value-info)
 * [test cases](#test_cases-info)
 
 ## Search info
@@ -31,8 +33,14 @@
 * Enter the path to the JSON file . for eg. ./client/client.json
 * Enter the field to search for . for eg. full_name
 
+## Search_json_value info
+* ruby search_json_value.rb
+* Enter the path to the JSON file . for eg. ./client/client.json
+* Enter the field to search for . for eg. full_name
+* Enter the value to search for . for eg. john
+
 ## Test Cases info
-* Four test cases added 1)spec/client.rb 2) spec/search.rb 3) spec/search_api.rb 4) spec/search_json.rb
-* run rspec spec/client.rb or spec/search.rb or spec/search_api.rb or spec/search_json.rb
-* run bundle exec rspec spec/client.rb or spec/search.rb or spec/search_api.rb or spec/search_json.rb
+* Four test cases added 1)spec/client.rb 2) spec/search.rb 3) spec/search_api.rb 4) spec/search_json.rb 5) spec/search_json_value.rb
+* run rspec spec/client.rb or spec/search.rb or spec/search_api.rb or spec/search_json.rb or spec/search_json_value.rb
+* run bundle exec rspec spec/client.rb or spec/search.rb or spec/search_api.rb or spec/search_json.rb or spec/search_json_value.rb
 * run spec/*
---
+++ spec/search_json_value.rb
@@ -0,0 +1,54 @@
+require 'spec_helper'
+require_relative '../search_json_value'
+
+describe 'parse_json_file' do
+  it 'should return a hash when given a valid JSON file' do
+    file_path = 'spec/fixtures/clients.json'
+    expect(parse_json_file(file_path).first(2)).to eq([{'id' => 1, 'full_name' => 'John Doe', 'email' => 'john.doe@gmail.com'}, {'id' => 2, 'full_name' => 'Jane Smith', 'email' => 'jane.smith@yahoo.com'}])
+  end
+
+  it 'should raise an error when given an invalid JSON file' do
+    file_path = 'spec/fixtures/invalid_clients.json'
+    expect(parse_json_file(file_path)) .to raise_error(StandardError, /Error reading JSON file:/)
+  end
+end
+
+describe 'search_json_value' do
+  it 'should return an array of results when given a valid JSON data and a field to search' do
+    json_data = [{ 'name' => 'John' }, { 'name' => 'Jane' }]
+    field_to_search = 'name'
+    value_to_search = 'john'
+    expect(search_data(json_data, field_to_search, value_to_search)).to eq([{ 'name' => 'John' }])
+  end
+
+  it 'should return an empty array when given an empty JSON data and a field to search' do
+    json_data = []
+    field_to_search = 'name'
+    value_to_search = 'john'
+    expect(search_data(json_data, field_to_search, value_to_search)).to eq([])
+  end
+
+  it 'should return an empty array when given a JSON data with no matching field' do
+    json_data = [{ 'name' => 'John' }, { 'age' => 30 }]
+    field_to_search = 'email'
+    value_to_search = 'john'
+    expect(search_data(json_data, field_to_search, value_to_search)).to eq([])
+  end
+end
+
+describe 'parse_json_file' do
+  it 'should print a message when given an invalid JSON file' do
+    allow($stdin).to receive(:gets).and_return('spec/fixtures/invalid_clients.json', 'name')
+    expect { parse_json_file('fixtures/invalid_clients.json') }.to output("Error: Invalid JSON file.\n").to_stdout
+  end
+
+  it 'should print a message when given a valid JSON file and no matching field' do
+    allow($stdin).to receive(:gets).and_return('spec/fixtures/clients.json', 'name')
+    expect { parse_json_file('fixtures/clients.json') }.to output("No results found for name.\n").to_stdout
+  end
+
+  it 'should print the results when given a valid JSON file and a matching field' do
+    allow($stdin).to receive(:gets).and_return('spec/fixtures/clients.json', 'name')
+    expect { parse_json_file('fixtures/clients.json') }.to output("Results for name:\nJohn\nJane\n").to_stdout
+  end
+end
